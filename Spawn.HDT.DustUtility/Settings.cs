b5033cb450d9e4b1b4f8f3737b5b5d436ead94f5
Chris-Lennart Junge <j.chris_lennart@live.de>
2017-06-28 22:46:23 +0200

    added application settings

--- Spawn.HDT.DustUtility/Settings.cs
+++ Spawn.HDT.DustUtility/Settings.cs
@@ -2,6 +2,17 @@
 {
     public static class Settings
     {
-        public static bool OfflineMode { get; set; }
+        public static bool OfflineMode
+        {
+            get
+            {
+                return Properties.Settings.Default.OfflineMode;
+            }
+            set
+            {
+                Properties.Settings.Default.OfflineMode = value;
+                Properties.Settings.Default.Save();
+            }
+        }
     }
 }
