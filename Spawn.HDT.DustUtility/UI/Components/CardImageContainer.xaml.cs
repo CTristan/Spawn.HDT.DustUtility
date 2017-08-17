87aa86e0c24c89dbd829f95a173fbb39ae0eb0fd
Chris-Lennart Junge <j.chris_lennart@live.de>
2017-08-17 20:10:28 +0200

    set current image to null after dispose

--- Spawn.HDT.DustUtility/UI/Components/CardImageContainer.xaml.cs
+++ Spawn.HDT.DustUtility/UI/Components/CardImageContainer.xaml.cs
@@ -71,6 +71,7 @@ namespace Spawn.HDT.DustUtility.UI.Components
                 if (m_currentImage != null)
                 {
                     m_currentImage.Dispose();
+                    m_currentImage = null;
                 }
                 else { }
 
