09580ed57824af6d5e88cbcc3bffd702a2fd3620
Chris-Lennart Junge <j.chris_lennart@live.de>
2017-08-27 00:12:33 +0200

    using new constructor for settings dialog

--- Spawn.HDT.DustUtility/DustUtilityPlugin.cs
+++ Spawn.HDT.DustUtility/DustUtilityPlugin.cs
@@ -2,6 +2,7 @@
 using Hearthstone_Deck_Tracker.API;
 using Hearthstone_Deck_Tracker.Plugins;
 using Hearthstone_Deck_Tracker.Utility.Logging;
+using Spawn.HDT.DustUtility.Net;
 using Spawn.HDT.DustUtility.Offline;
 using Spawn.HDT.DustUtility.UI;
 using Spawn.HDT.DustUtility.UI.Dialogs;
@@ -70,7 +71,7 @@ namespace Spawn.HDT.DustUtility
         {
             Log.WriteLine("Opening settings dialog", LogType.Debug);
 
-            SettingsDialog dialog = new SettingsDialog();
+            SettingsDialog dialog = new SettingsDialog(Directory.Exists(Path.Combine(DataDirectory, HearthstoneCardImageManager.CacheFolderName)));
 
             dialog.ShowDialog();
 
